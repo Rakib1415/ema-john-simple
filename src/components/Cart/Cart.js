838d71d273a5b0e2e21cf041d8b04def5c96559c
Rakib1415 <rakibiuaece@gmail.com>
2020-03-13 01:40:34 +0600

    ema jhon done

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+   // const total = cart.reduce((total, prd) => total + prd.price, 0);
+   let total = 0;
+   for(let i = 0; i< cart.length; i++){
+       const product = cart[i];
+       total = total + product.price;
+   }
+   let shipping = 0;
+   if(total > 35){
+       shipping = 0;
+   }
+   else if(total > 15){
+       shipping = 4.99;
+   }
+   else if(total >0){
+       shipping = 12.99;
+   }
+   const tax = Math.round(total/10);
+    return (
+        <div>
+                <h3>Order Summary</h3>
+                <p>Items Ordered :{cart.length} </p>
+                <p>Product price :{total}</p>
+                <p><small>Shipping Cost : {shipping}</small></p>
+                <p><small>Tax or Vat : {tax}</small></p>
+                <p>Total Price : {total + shipping + tax}</p>
+        </div>
+    );
+};
+
+export default Cart;
